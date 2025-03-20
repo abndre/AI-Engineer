d5e641a6743aa07b2c104f98a3c3a53be36f7f33
Andre Santos Barros da Silva <andrezio@live.com>
2025-03-20 17:36:04 -0300

    cuda

--- hugging-face-dev/testando_token.py
+++ hugging-face-dev/testando_token.py
@@ -1,13 +1,18 @@
-from transformers import pipeline
-from transformers import AutoTokenizer,AutoModelForSequenceClassification
+import torch
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+
 # Download the model and tokenizer
 model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 
-# Create the pipeline
-sentimentAnalysis = pipeline(task="sentiment-analysis", model=model, tokenizer=tokenizer)
-input="I love using Hugging Face models!"
+# Verifica se a GPU está disponível
+device = 0 if torch.cuda.is_available() else -1
+
+# Create the pipeline with GPU support
+sentimentAnalysis = pipeline(task="sentiment-analysis", model=model, tokenizer=tokenizer, device=device)
+
+input_text = "I love using Hugging Face models!"
 # Predict the sentiment
-output = sentimentAnalysis(input)
+output = sentimentAnalysis(input_text)
 
 print(f"Sentiment using AutoClasses: {output[0]['label']}")
