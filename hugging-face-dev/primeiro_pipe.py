e90c09c1dafacd7bdff02d2a7d4712a6572fd3af
Andre Santos Barros da Silva <andrezio@live.com>
2025-03-21 11:10:35 -0300

    text class

--- hugging-face-dev/primeiro_pipe.py
+++ hugging-face-dev/primeiro_pipe.py
@@ -1,3 +1,4 @@
+print("===== parte 1 ======")
 from transformers import pipeline
 
 # Criando o pipeline de análise de sentimentos
@@ -9,6 +10,8 @@ result = sentiment_pipeline(text)
 
 print(result)
 
+print("===== parte 2 ======")
+
 # Create a pipeline
 classifier = pipeline(
   task="text-classification", 
@@ -19,3 +22,27 @@ classifier = pipeline(
 output = classifier("I will walk dog")
 
 print(output)
+
+print("===== parte 3 ======")
+
+
+# Create the pipeline
+classifier = pipeline(task="text-classification", model="cross-encoder/qnli-electra-base")
+
+# Predict the output
+output = classifier("Where is the capital of France?, Brittany is known for their kouign-amann.")
+
+print(output)
+
+print("===== parte 4 ======")
+text='A 75-million-year-old Gorgosaurus fossil is the first tyrannosaur skeleton ever found with a filled stomach.'
+# Build the zero-shot classifier
+classifier = pipeline(task="zero-shot-classification", model="facebook/bart-large-mnli")
+
+# Create the list
+candidate_labels = ["politics", "science", "sports"]
+
+# Predict the output
+output = classifier(text, candidate_labels)
+
+print(f"Top Label: {output['labels'][0]} with score: {output['scores'][0]}")
